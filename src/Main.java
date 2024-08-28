5fc5060ebf24e1e08bc6197e3c983ea560ae2079
FranRevi8 <fcorevidiego@gmail.com>
2024-08-28 17:52:10 +0200

    minor changes

--- src/Main.java
+++ src/Main.java
@@ -19,68 +19,74 @@ public class Main {
 
         for (CharacterType character : CharacterType.values()) {
             System.out.println(character.ordinal() + "-" + character.name());
-        };
+        }
+        ;
 
         int response = scanner.nextInt();
+        scanner.nextLine();
 
-        System.out.println("El tipo de personaje elegido es " + CharacterType.values()[response]);
-        System.out.println("¿Como llamamos a este personaje?");
+        System.out.println("\nEl tipo de personaje elegido es " + CharacterType.values()[response]);
+        System.out.println("\n¿Como llamamos a este personaje?");
 
-        name = scanner.next();
+        name = scanner.nextLine();
 
         System.out.println("\nEl nombre elegido es  " + name);
 
-        switch (CharacterType.values()[response]){
+        switch (CharacterType.values()[response]) {
             case WIZARD:
                 int mana = 10 + random.nextInt(41);
                 int intelligence = 1 + random.nextInt(50);
                 hp = 100 + random.nextInt(101);
-                character1 = new Wizard(name,CharacterType.WIZARD,hp,mana,intelligence);
+                character1 = new Wizard(name, CharacterType.WIZARD, hp, mana, intelligence);
                 break;
             case WARRIOR:
                 int strength = 1 + random.nextInt(10);
                 int stamina = 10 + random.nextInt(41);
                 hp = 100 + random.nextInt(101);
-                character1 = new Warrior(name,CharacterType.WARRIOR,hp,strength,stamina);
+                character1 = new Warrior(name, CharacterType.WARRIOR, hp, strength, stamina);
                 break;
             default:
                 character1 = null;
-        };
-
-        System.out.println(character1);
-
+        }
+        ;
 
         System.out.println("\nElige al segundo personaje:");
 
         for (CharacterType character : CharacterType.values()) {
             System.out.println(character.ordinal() + "-" + character.name());
-        };
+        }
+        ;
 
         response = scanner.nextInt();
+        scanner.nextLine();
 
-        System.out.println("El tipo de personaje elegido es " + CharacterType.values()[response]);
-        System.out.println("¿Como llamamos a este personaje?");
+        System.out.println("\nEl tipo de personaje elegido es " + CharacterType.values()[response]);
+        System.out.println("\n¿Como llamamos a este personaje?");
 
-        name = scanner.next();
+        name = scanner.nextLine();
 
         System.out.println("\nEl nombre elegido es  " + name);
 
-        switch (CharacterType.values()[response]){
+        switch (CharacterType.values()[response]) {
             case WIZARD:
                 int mana = 10 + random.nextInt(41);
                 int intelligence = 1 + random.nextInt(50);
                 hp = 100 + random.nextInt(101);
-                character2 = new Wizard(name,CharacterType.WIZARD,hp,mana,intelligence);
+                character2 = new Wizard(name, CharacterType.WIZARD, hp, mana, intelligence);
                 break;
             case WARRIOR:
                 int strength = 1 + random.nextInt(10);
                 int stamina = 10 + random.nextInt(41);
                 hp = 100 + random.nextInt(101);
-                character2 = new Warrior(name,CharacterType.WARRIOR,hp,strength,stamina);
+                character2 = new Warrior(name, CharacterType.WARRIOR, hp, strength, stamina);
                 break;
             default:
                 character2 = null;
-        };
+        }
+        ;
+
+        System.out.println("\n¡Que comience la batalla entre " + character1.getName() + " y " + character2.getName() + "!");
+
 
     }
 }
